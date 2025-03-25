169d0bda44dbd532906cf893af5d488d51ede23f
gofreelee <19794320270@qq.com>
2025-03-25 04:06:49 +0000

    add enable mps script

---
+++ enbale_mps.sh
@@ -0,0 +1,10 @@
+export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps  # 设置 MPS 管道目录
+export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log   # 设置 MPS 日志目录
+
+mkdir -p $CUDA_MPS_PIPE_DIRECTORY
+mkdir -p $CUDA_MPS_LOG_DIRECTORY
+
+nvidia-cuda-mps-control -d
+
+
+
